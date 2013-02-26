1e1e292a757830466b382a20a95cd3b1c9538ec9
Emely <cloudyerout@gmail.com>
2013-02-26 14:42:03 -0500

    Update Emely/connect.php

--- Emely/connect.php
+++ Emely/connect.php
@@ -11,23 +11,4 @@ if (!$link) {
     die('Error Connecting: (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
 }
-
-/*echo 'YOU ARE CONNECTED!...  using ' . mysqli_get_host_info($link) . "\n";
-echo '<br><br>';
-
-$result = $link->query("SELECT firstName, lastName, gpa, major FROM students WHERE gpa > 3");
-if($result){
-     // Cycle through results
-    while ($row = $result->fetch_assoc()){
-        printf ("%s , %s , %s (%s)\n", 
-               $row["firstName"], $row["lastName"],$row["gpa"], $row["major"]); 
-	echo '<br>';
-    }
-
-    // Free result set
-    $result->close();
-    $link->next_result();
-}
-mysqli_close($link);
-*/
 ?>
